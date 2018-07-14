73ae4fef1bd5f9e59e9c12b117d20e2508ae0e2d
Parthasarathy Viswanathan <sarathy.v@gmail.com>
2018-07-13 18:12:00 -0700

    First checkin

--- JavaStreams/src/com/company/algonds/java/LambdaExpr.java
+++ JavaStreams/src/com/company/algonds/java/LambdaExpr.java
@@ -1,4 +1,13 @@
 package com.company.algonds.java;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class LambdaExpr {
+    public static void main(String[] args) {
+        List<String> input = Arrays.asList("apple","blueberry","catlin","alligator");
+        Collections.sort(input, ( a,  b) -> a.compareTo(b));
+        System.out.println(input);
+    }
 }
